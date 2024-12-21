37dd9815c45d386c6d167ed2c54ed0ab7eaaf50a
Nicolai Dam <nicolai.dam@ufst.dk>
2024-12-21 21:50:29 +0100

    Add event controller

--- src/main/kotlin/dk/example/feedback/controller/EventController.kt
+++ src/main/kotlin/dk/example/feedback/controller/EventController.kt
@@ -1,11 +1,10 @@
 package dk.example.feedback.controller
 
+import dk.example.feedback.constants.Roles
 import dk.example.feedback.model.EventInput
 import dk.example.feedback.model.dto.ManagerEventDto
 import dk.example.feedback.model.dto.ParticipantEventDto
-import dk.example.feedback.model.dto.SessionDto
 import dk.example.feedback.service.EventService
-import dk.example.feedback.service.SessionService
 import org.springframework.web.bind.annotation.*
 import java.util.*
 import org.springframework.security.access.prepost.PreAuthorize
@@ -19,27 +18,27 @@ class EventController(
 ) {
 
     @PostMapping
-    @PreAuthorize("hasAuthority('Manager')")
-    fun createEvent(@RequestBody eventInput: EventInput, @AuthenticationPrincipal principal: Jwt): ManagerEventDto {
-        return eventService.create(eventInput = eventInput, accountId = principal.subject)
+    @PreAuthorize("hasAuthority('${Roles.MANAGER}')")
+    fun createEvent(@RequestBody eventInput: EventInput): ManagerEventDto {
+        return eventService.createEvent(eventInput = eventInput)
     }
 
     @PutMapping("/{eventId}")
-    @PreAuthorize("hasAuthority('Manager')")
-    fun updateEvent(@RequestBody eventInput: EventInput, @PathVariable eventId: UUID, @AuthenticationPrincipal principal: Jwt): ManagerEventDto {
-        val accountId = principal.subject
-        return eventService.update(eventInput = eventInput, eventId = eventId, accountId = accountId)
+    @PreAuthorize("hasAuthority('${Roles.MANAGER}')")
+    fun updateEvent(@RequestBody eventInput: EventInput, @PathVariable eventId: UUID): ManagerEventDto {
+        return eventService.updateEvent(eventInput = eventInput, eventId = eventId)
     }
 
     @DeleteMapping("/{eventId}")
-    @PreAuthorize("hasAuthority('Manager')")
+    @PreAuthorize("hasAuthority('${Roles.MANAGER}')")
     fun deleteEvent(@PathVariable eventId: UUID, @AuthenticationPrincipal principal: Jwt) {
-        return eventService.delete(eventId = eventId, accountId = principal.subject)
+        return eventService.deleteEvent(eventId = eventId)
     }
 
-    @PostMapping("attending/{eventId}")
-    @PreAuthorize("hasAuthority('Manager') or hasAuthority('Participant')")
-    fun acceptEvent(@PathVariable eventId: UUID, @AuthenticationPrincipal principal: Jwt): ParticipantEventDto {
-        return eventService.acceptEventInvite(eventId = eventId, accountId = principal.subject)
+    @PostMapping("join/{eventCode}")
+    @PreAuthorize("hasAuthority('${Roles.MANAGER}') or hasAuthority('${Roles.PARTICIPANT}')")
+    fun acceptEvent(@PathVariable eventCode: String, @AuthenticationPrincipal principal: Jwt): ParticipantEventDto {
+        return eventService.joinEvent(eventCode = eventCode)
     }
 }
+
