4dcf2362539f3d0c656b0696404a9ef51aef3b32
Nicolai Dam <nicolai.dam@ufst.dk>
2024-12-21 22:23:01 +0100

    Add session controller and service

--- src/main/kotlin/dk/example/feedback/controller/SessionController.kt
+++ src/main/kotlin/dk/example/feedback/controller/SessionController.kt
@@ -1,11 +1,8 @@
 package dk.example.feedback.controller
 
-import dk.example.feedback.helpers.getCustomClaim
 import dk.example.feedback.model.dto.SessionDto
 import dk.example.feedback.service.SessionService
 import org.springframework.web.bind.annotation.*
-import org.springframework.security.core.annotation.AuthenticationPrincipal
-import org.springframework.security.oauth2.jwt.Jwt
 
 @RestController
 @RequestMapping(ControllerPaths.Session)
