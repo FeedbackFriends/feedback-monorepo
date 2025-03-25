0ea69e38116c22df8a9967a95a14f58f470fc3be
Nicolai Dam <nicolai.dam@ufst.dk>
2025-03-25 22:12:56 +0100

    Add activity controller instead of having mark endpoint in session controller

--- apps/api/src/main/kotlin/dk/example/feedback/controller/ActivityController.kt
+++ apps/api/src/main/kotlin/dk/example/feedback/controller/ActivityController.kt
@@ -1,9 +1,7 @@
 package dk.example.feedback.controller
 
-import dk.example.feedback.dto.SessionDto
-import dk.example.feedback.dto.UpdatedSessionDto
 import dk.example.feedback.model.enumerations.RoleConstants
-import dk.example.feedback.service.SessionService
+import dk.example.feedback.service.ActivityService
 import io.swagger.v3.oas.annotations.tags.Tag
 import org.springframework.security.access.prepost.PreAuthorize
 import org.springframework.security.core.annotation.AuthenticationPrincipal
@@ -13,24 +11,14 @@ import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
-@Tag(name = "Session")
-@RequestMapping("/session")
-class SessionController(val sessionService: SessionService) {
-
-    @GetMapping
-    @PreAuthorize("isAuthenticated()")
-    fun getSession(
-        @AuthenticationPrincipal principal: Jwt
-    ): SessionDto {
-        return sessionService.getSession(jwt = principal)
-    }
-
-    @GetMapping("/real-time-updates")
+@Tag(name = "Activity")
+@RequestMapping("/activity")
+class ActivityController(val activityService: ActivityService) {
+    @GetMapping("/mark-activity-as-seen")
     @PreAuthorize("hasAuthority('${RoleConstants.ORGANIZER}')")
-    fun getUpdatedSession(
+    fun markActivityAsSeen(
         @AuthenticationPrincipal principal: Jwt
-    ): UpdatedSessionDto {
-        return sessionService.getUpdatedSession(jwt = principal)
+    ) {
+        return activityService.markActivityAsSeen(jwt = principal)
     }
 }
-
--- apps/api/src/main/kotlin/dk/example/feedback/service/SessionService.kt
+++ apps/api/src/main/kotlin/dk/example/feedback/service/SessionService.kt
@@ -1,16 +1,11 @@
 package dk.example.feedback.service
 
-import dk.example.feedback.dto.ActivityDto
-import dk.example.feedback.dto.ActivityItem
 import dk.example.feedback.dto.SessionDto
 import dk.example.feedback.dto.UpdatedSessionDto
 import dk.example.feedback.helpers.getAccountId
 import dk.example.feedback.helpers.role
-import dk.example.feedback.model.database.EventEntity
 import dk.example.feedback.model.enumerations.Role
-import dk.example.feedback.persistence.repo.ActivityRepo
 import dk.example.feedback.persistence.repo.EventRepo
-import dk.example.feedback.persistence.repo.NewFeedbackNotificationRepo
 import org.slf4j.LoggerFactory
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.stereotype.Service
@@ -19,21 +14,21 @@ import org.springframework.stereotype.Service
 class SessionService(
     val eventService: EventService,
     val accountService: AccountService,
-    val newFeedbackNotificationRepo: NewFeedbackNotificationRepo,
-    val activityRepo: ActivityRepo,
-    private val eventRepo: EventRepo,
+    val eventRepo: EventRepo,
+    val activityService: ActivityService,
 ) {
 
     private val logger = LoggerFactory.getLogger(SessionService::class.java)
 
     fun getUpdatedSession(jwt: Jwt): UpdatedSessionDto {
-        val newActivityItems = movePendingNotificationsToActivityAndReturn(accountId = jwt.getAccountId())
+        val newActivityItems =
+            activityService.movePendingNotificationsToActivityAndReturn(accountId = jwt.getAccountId())
         logger.info("New feedback notifications: ${newActivityItems.size} for user: ${jwt.getAccountId()}")
         return UpdatedSessionDto(
             events = newActivityItems.map {
                 it.toManagerEvent(pinCode = eventRepo.getPinCodeForEvent(it.id))
             },
-            activity = getActivity(accountId = jwt.getAccountId())
+            activity = activityService.getActivity(accountId = jwt.getAccountId())
         )
     }
 
@@ -46,7 +41,7 @@ class SessionService(
         logger.info("Get session with role: $role")
         when (role) {
             Role.Organizer -> {
-                movePendingNotificationsToActivityAndReturn(accountId = accountId)
+                activityService.movePendingNotificationsToActivityAndReturn(accountId = accountId)
                 val managerEvents = eventService.getManagerEvents(accountId)
                 val session = SessionDto(
                     role = role,
@@ -58,7 +53,7 @@ class SessionService(
                     participantEvents = participantEvents,
                     managerData = SessionDto.ManagerDataDto(
                         managerEvents = managerEvents,
-                        activity = getActivity(accountId = accountId)
+                        activity = activityService.getActivity(accountId = accountId)
                     )
                 )
                 return session
@@ -89,43 +84,5 @@ class SessionService(
                 )
             }
         }
-    }
-
-    fun markActivityAsSeen(jwt: Jwt) {
-        val accountId = jwt.getAccountId()
-        activityRepo.markAllAsSeen(accountId = accountId)
-    }
-
-    private fun getActivity(accountId: String): ActivityDto {
-        val items = activityRepo.listAllForAccount(accountId = accountId).map {
-            ActivityItem(
-                id = it.id,
-                date = it.createdAt,
-                eventTitle = it.event.title,
-                eventId = it.event.id,
-                newFeedbackCount = it.newFeedback,
-                seenBefore = it.seenBefore
-            )
-        }
-        return ActivityDto(
-            items = items,
-            unseenTotal = items.filter { !it.seenBefore }.size
-        )
-    }
-
-    private fun movePendingNotificationsToActivityAndReturn(accountId: String): List<EventEntity> {
-        val pendingNewFeedbackNotifications = newFeedbackNotificationRepo.getAllForAccount(accountId = accountId)
-        logger.info("Pending new feedback notifications: ${pendingNewFeedbackNotifications.size}")
-        newFeedbackNotificationRepo.removeAllForAccount(accountId = accountId)
-        logger.info("Removed all pending new feedback notifications for account: $accountId")
-        for (feedback in pendingNewFeedbackNotifications) {
-            logger.info("Persisting activity feed for event: ${feedback.event.id}")
-            activityRepo.persistActivity(
-                accountId = accountId,
-                eventId = feedback.event.id,
-                newFeedback = feedback.newFeedback,
-            )
-        }
-        return pendingNewFeedbackNotifications.map { it.event }
     }
 }
