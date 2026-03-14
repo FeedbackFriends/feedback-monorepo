f2d066279ca14810c01682b6b32fde04ce83156e
Nicolai Dam <nicolaidam96@gmail.com>
2026-03-14 10:05:37 +0100

    [backend] Simplify dependency management overrides

--- backend/build.gradle.kts
+++ backend/build.gradle.kts
@@ -7,35 +7,9 @@ plugins {
 }
 
 val versionCatalog = extensions.getByType<VersionCatalogsExtension>().named("libs")
-val assertjVersion = versionCatalog.findVersion("assertj").get().requiredVersion
-val commonsCompressVersion = versionCatalog.findVersion("commonsCompress").get().requiredVersion
 val commonsLang3Version = versionCatalog.findVersion("commonsLang3").get().requiredVersion
-val grpcVersion = versionCatalog.findVersion("grpc").get().requiredVersion
 val jacksonVersion = versionCatalog.findVersion("jackson").get().requiredVersion
-val logbackVersion = versionCatalog.findVersion("logback").get().requiredVersion
-val forcedDependencies =
-    listOf(
-        "org.apache.commons:commons-compress:$commonsCompressVersion",
-    ) +
-        listOf(
-            "grpc-alts",
-            "grpc-api",
-            "grpc-auth",
-            "grpc-context",
-            "grpc-core",
-            "grpc-googleapis",
-            "grpc-grpclb",
-            "grpc-inprocess",
-            "grpc-netty-shaded",
-            "grpc-opentelemetry",
-            "grpc-protobuf",
-            "grpc-protobuf-lite",
-            "grpc-rls",
-            "grpc-services",
-            "grpc-stub",
-            "grpc-util",
-            "grpc-xds",
-        ).map { "io.grpc:$it:$grpcVersion" }
+val testcontainersVersion = versionCatalog.findVersion("testcontainers").get().requiredVersion
 
 repositories {
     mavenCentral()
@@ -55,18 +29,12 @@ subprojects {
         extensions.configure<DependencyManagementExtension>("dependencyManagement") {
             imports {
                 mavenBom("com.fasterxml.jackson:jackson-bom:$jacksonVersion")
+                mavenBom("org.testcontainers:testcontainers-bom:$testcontainersVersion")
             }
 
             dependencies {
-                dependency("org.assertj:assertj-core:$assertjVersion")
                 dependency("org.apache.commons:commons-lang3:$commonsLang3Version")
-                dependency("ch.qos.logback:logback-classic:$logbackVersion")
-                dependency("ch.qos.logback:logback-core:$logbackVersion")
             }
         }
-    }
-
-    configurations.configureEach {
-        resolutionStrategy.force(*forcedDependencies.toTypedArray())
     }
 }
